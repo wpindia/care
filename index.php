ce9aadaca530c4e09829c126a70d05631d89a444
ninad-jobsforher <ninad@jobsforher.com>
2017-12-18 10:28:15 +0530

    index home page

---
+++ index.php
@@ -0,0 +1,363 @@
+<head>
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js">
+	</script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
+	
+</head>
+<body>
+<div class="wrapper" id="">
+		
+		<div class="header">
+			<img src="images/logo.png" height="100px" width="100px" />
+		</div>	
+
+
+		<div class="care-slider-wrapper">
+			<div class="care-slider">
+			  <div><img src="images/1.jpg"/></div>
+			  <div><img src="images/2.jpg"/></div>
+			  <div><img src="images/3.jpg"/></div>
+			</div>
+		</div>
+
+		<div class="row card-wrapper">
+
+			<div id="" class="care-card-container">
+				<div class="care-card">
+					<div class="poster-container">
+						<a href="" title="" onclick="" data-id="">
+							<img data-src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-mobile="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-error="https://in.bmscdn.com/webin/static/nopreview.jpg" alt="" class="poster-image" src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg">
+						</a>		
+					</div>
+					
+					<div class="detail">
+						<div class="">
+							<div class="">
+								<h2>
+									<a href="" title="" onclick="" data-id="plays-Amar-Photo-Studio">Title 1</a>
+								</h2>
+								<div class=""><a href="">City</a></div>
+								
+							</div>
+						</div>
+						<div class="inner-details">
+							
+							<div class="buy-now">
+								<a class="btn _cuatro" href="" onclick="">Know more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="" class="care-card-container">
+				<div class="care-card">
+					<div class="poster-container">
+						<a href="" title="" onclick="" data-id="">
+							<img data-src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-mobile="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-error="https://in.bmscdn.com/webin/static/nopreview.jpg" alt="" class="poster-image" src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg">
+						</a>		
+					</div>
+					
+					<div class="detail">
+						<div class="">
+							<div class="">
+								<h2>
+									<a href="" title="" onclick="" data-id="plays-Amar-Photo-Studio">Title 1</a>
+								</h2>
+								<div class=""><a href="">Address</a></div>
+								
+							</div>
+						</div>
+						<div class="inner-details">
+							
+							<div class="buy-now">
+								<a class="btn _cuatro" href="" onclick="">Know more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="" class="care-card-container">
+				<div class="care-card">
+					<div class="poster-container">
+						<a href="" title="" onclick="" data-id="">
+							<img data-src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-mobile="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-error="https://in.bmscdn.com/webin/static/nopreview.jpg" alt="" class="poster-image" src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg">
+						</a>		
+					</div>
+					
+					<div class="detail">
+						<div class="">
+							<div class="">
+								<h2>
+									<a href="" title="" onclick="" data-id="plays-Amar-Photo-Studio">Title 1</a>
+								</h2>
+								<div class=""><a href="">Address</a></div>
+								
+							</div>
+						</div>
+						<div class="inner-details">
+							
+							<div class="buy-now">
+								<a class="btn _cuatro" href="" onclick="">Know more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="" class="care-card-container">
+				<div class="care-card">
+					<div class="poster-container">
+						<a href="" title="" onclick="" data-id="">
+							<img data-src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-mobile="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-error="https://in.bmscdn.com/webin/static/nopreview.jpg" alt="" class="poster-image" src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg">
+						</a>		
+					</div>
+					
+					<div class="detail">
+						<div class="">
+							<div class="">
+								<h2>
+									<a href="" title="" onclick="" data-id="plays-Amar-Photo-Studio">Title 1</a>
+								</h2>
+								<div class=""><a href="">Address</a></div>
+								
+							</div>
+						</div>
+						<div class="inner-details">
+							
+							<div class="buy-now">
+								<a class="btn _cuatro" href="" onclick="">Know more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="" class="care-card-container">
+				<div class="care-card">
+					<div class="poster-container">
+						<a href="" title="" onclick="" data-id="">
+							<img data-src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-mobile="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-error="https://in.bmscdn.com/webin/static/nopreview.jpg" alt="" class="poster-image" src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg">
+						</a>		
+					</div>
+					
+					<div class="detail">
+						<div class="">
+							<div class="">
+								<h2>
+									<a href="" title="" onclick="" data-id="plays-Amar-Photo-Studio">Title 1</a>
+								</h2>
+								<div class=""><a href="">Address</a></div>
+								
+							</div>
+						</div>
+						<div class="inner-details">
+							
+							<div class="buy-now">
+								<a class="btn _cuatro" href="" onclick="">Know more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="" class="care-card-container">
+				<div class="care-card">
+					<div class="poster-container">
+						<a href="" title="" onclick="" data-id="">
+							<img data-src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-mobile="//in.bmscdn.com/events/eventlisting/ET00049215.jpg" data-error="https://in.bmscdn.com/webin/static/nopreview.jpg" alt="" class="poster-image" src="//in.bmscdn.com/events/eventlisting/ET00049215.jpg">
+						</a>		
+					</div>
+					
+					<div class="detail">
+						<div class="">
+							<div class="">
+								<h2>
+									<a href="" title="" onclick="" data-id="plays-Amar-Photo-Studio">Title 1</a>
+								</h2>
+								<div class=""><a href="">Address</a></div>
+								
+							</div>
+						</div>
+						<div class="inner-details">
+							
+							<div class="buy-now">
+								<a class="btn _cuatro" href="" onclick="">Know more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+
+		<div class="footer-container">
+			Proudly Developed in India.<br/>
+			Copyright 2017 &copy Day-Care.in. All Rights Reserved.<br/>
+			The content and images used on this site are copyright protected and copyrights vests with the respective owners.
+		</div>
+	
+</div>
+	<script type="text/javascript">
+		$(document).ready(function(){
+		  $('.care-slider').slick({
+		      dots: true,
+			  infinite: false,
+			  speed: 300,
+			  slidesToShow: 1,
+			  slidesToScroll: 1,
+			  autoplay: true,
+  			  autoplaySpeed: 5000,
+  			  arrows: false,
+  			  //asNavFor: '.slider-for',
+  			  responsive: [
+			    {
+			      breakpoint: 1024,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1,
+			        infinite: true,
+			        dots: true
+			      }
+			    },
+			    {
+			      breakpoint: 600,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 2
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    }
+			    // You can unslick at a given breakpoint now by adding:
+			    // settings: "unslick"
+			    // instead of a settings object
+			  ]
+			});
+				
+		  });
+		
+	</script>	
+</body>			
+<style>
+.clear{
+	clear: both;
+}
+.footer-container {
+    background: #333338;
+    text-align: center;
+    color: white;
+    padding: 20px
+}    
+.care-slider-wrapper{
+	margin: 20px auto;
+    overflow: hidden;
+    height: 400px;
+}
+.care-slider{
+	height: 100%;
+}
+
+.slick-list{
+	display: block;
+    overflow: hidden;
+    height: 100%;
+    margin: 0;
+}
+
+body{
+	background: #f2f2f2;
+}
+.detail{    
+	transition: all .5s ease;
+    position: relative;
+    height: 195px!important;
+    overflow: hidden;
+    background: #fff;
+    padding: 0px 10px
+}
+.care-card{
+	text-align: left;
+    width: 100%;
+    position: relative;
+    min-height: 350px;
+    background-color: #fff;
+}
+.care-card-container{
+	width: 25%;
+    float: left;
+    margin: 0px 0px 25px 0px;
+    padding: 0px 12px;
+    box-sizing: border-box;
+}
+
+.wrapper{
+	max-width: 1400px;
+    width: 100%;
+    margin: 0px auto;
+}
+
+.poster-container{
+	transition: all .5s ease;
+    height: 200px;
+    width: 100%;
+    position: relative;
+    background-size: cover!important;
+    background-position: center;
+}
+
+.poster-image{
+	height: 100%;
+    width: 100%;
+    display: block;
+    color: #FFF;
+}
+.slick-dots {
+    position: absolute;
+    width: 100%;
+    text-align: center;
+    bottom: 20px;
+    padding: 0;
+    list-style: none;
+}
+
+.slick-dots li {
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+    width: 8px;
+    height: 8px;
+    margin: 0 5px;
+    border-radius: 50%;
+    padding: 0;
+    background-color: #FFF;
+    opacity: .25;
+}
+
+.slick-dots li.slick-active{
+	opacity:1;
+	background-color:#fff;
+}
+
+.slick-dots li button {
+    cursor: pointer;
+    display: block;
+    font-size: 0;
+    line-height: 0;
+    width: 10px;
+    height: 10px;
+    padding: 5px;
+    border: 0;
+    outline: 0;
+    background: 0 0;
+}
+
+</style>
+
